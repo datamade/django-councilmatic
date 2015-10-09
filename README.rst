9d6fc807cd287cf34e3e077c848642779df86328
Eric van Zanten <eric.vanzanten@gmail.com>
2015-10-09 16:25:05 -0500

    OK, this looks initial

---
+++ README.rst
@@ -0,0 +1,35 @@
+Councilmatic
+====================
+
+Reusable Django app providing the core functions for Councilmatic
+
+Team
+----
+
+-  David Moore - project manager
+-  Forest Gregg - Open Civic Data (OCD) and Legistar scraping
+-  Cathy Deng - data models and loading
+-  Derek Eder - front end
+-  Eric van Zanten - search and dev ops
+
+Errors / Bugs
+-------------
+
+If something is not behaving intuitively, it is a bug, and should be
+reported. Report it here:
+https://github.com/datamade/chi-councilmatic/issues
+
+Note on Patches/Pull Requests
+-----------------------------
+
+-  Fork the project.
+-  Make your feature addition or bug fix.
+-  Commit, do not mess with rakefile, version, or history.
+-  Send a pull request. Bonus points for topic branches.
+
+Copyright
+---------
+
+Copyright (c) 2015 Participatory Politics Foundation and DataMade.
+Released under the `MIT
+License <https://github.com/datamade/chi-councilmatic/blob/master/LICENSE>`__.
---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-councilmatic',
+    version='0.1.1',
+    packages=['councilmatic_core'],
+    include_package_data=True,
+    license='MIT License',  # example license
+    description='Core functions for councilmatic.org family',
+    long_description=README,
+    url='http://councilmatic.org/',
+    author='DataMade, LLC',
+    author_email='info@datamade.us',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License', # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        # Replace these appropriately if you are stuck on Python 2.
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
