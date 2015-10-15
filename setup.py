f6e495f562c0a05e1178fa563e3a110ada2fe13d
Cathy Deng <cathyd125@gmail.com>
2015-10-15 12:21:02 -0500

    bump version to 0.2.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-councilmatic',
-    version='0.2',
+    version='0.2.1',
     packages=['councilmatic_core'],
     include_package_data=True,
     license='MIT License',  # example license
