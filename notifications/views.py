e3ea17252ec4f11a9da65550cee9f8bafafdcb0f
Michael Castelle <mc-github@perquackey.com>
2016-08-15 14:12:41 -0500

    fixed 'signup not saving password' bug

--- notifications/views.py
+++ notifications/views.py
@@ -56,7 +56,7 @@ class CouncilmaticUserCreationForm(UserCreationForm):
         fields = ("username", "email", "password1", "password2")
 
     def save(self, commit=True):
-        user = super(UserCreationForm, self).save(commit=False)
+        user = super().save(commit=False) # this should set the password
         user.email = self.cleaned_data["email"]
         if commit:
             user.save()
