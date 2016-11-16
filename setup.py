039df440c0d455927ec16b8f6434c90fb65fa23f
reginafcompton <reginafcompton@gmail.com>
2016-11-16 13:54:01 -0600

    Update release

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-councilmatic',
-    version='0.6.17',
+    version='0.6.18',
     packages=['councilmatic_core'],
     include_package_data=True,
     license='MIT License',  # example license
