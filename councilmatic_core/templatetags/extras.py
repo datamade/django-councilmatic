c191006c3de88bfc1f426159c6b4358f632c45ba
reginafcompton <reginafcompton@gmail.com>
2016-11-16 14:21:50 -0600

    Update the search functionality

--- councilmatic_core/templatetags/extras.py
+++ councilmatic_core/templatetags/extras.py
@@ -146,7 +146,10 @@ def custom_reverse_search_url(subscription):
 
 @register.filter
 def format_url_parameters(url):
-    params = {"?&sort_by=date": "", "?&sort_by=title": "", "?&sort_by=relevance": ""}
+    params = {"?&sort_by=date": "",\
+        "?&sort_by=title": "", "?&sort_by=relevance": "",\
+        "&sort_by=date": "", "&sort_by=title": "",\
+        "&sort_by=relevance": ""}
 
     paramsDict = dict((re.escape(k), v) for k, v in params.items())
     pattern = re.compile("|".join(paramsDict.keys()))
