705fdb57a4cdbbb765afcae7c25e8095822ed309
reginafcompton <reginafcompton@gmail.com>
2018-03-30 16:22:00 -0500

    Use textract to convert pdf, doc, and docx files

--- councilmatic_core/management/commands/convert_attachment_text.py
+++ councilmatic_core/management/commands/convert_attachment_text.py
@@ -7,6 +7,8 @@ import sqlalchemy as sa
 import datetime
 import signal
 import os
+import requests
+import textract
 
 from django.core.management.base import BaseCommand
 from django.conf import settings
@@ -26,12 +28,21 @@ engine = sa.create_engine(DB_CONN.format(**settings.DATABASES['default']),
 
 class Command(BaseCommand):
     help = 'Converts bill attachments into plain text'
+    
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--update_all',
+            default=False,
+            action='store_true',
+            help='Add or update plain text for all bill attachments.')
 
     def handle(self, *args, **options):
+        self.update_all = options['update_all']
         self.connection = engine.connect()
 
         self.add_plain_text()
         
+        # Unoconv solution!
         # listener = subprocess.Popen(['unoconv', '--listener'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         # try:
         #     self.add_html()
@@ -41,17 +52,29 @@ class Command(BaseCommand):
     def get_document_url(self):
         self.connection.execute("SET local timezone to '{}'".format(settings.TIME_ZONE))
         with engine.begin() as connection:
-            # Only apply this query to most recently updated (or created) bills.
-            # max_updated = BillDocument.objects.all().aggregate(Max('ocd_updated_at'))['ocd_updated_at__max']
+            # Only apply this query to most recently updated (or created) bill documents.
+            max_updated = BillDocument.objects.all().aggregate(Max('updated_at'))['updated_at__max']
 
-            query = '''
-                SELECT id, url
-                FROM councilmatic_core_billdocument 
-                WHERE document_type='A' 
-                AND full_text is null
-                ORDER BY updated_at DESC
-            '''
-            result = connection.execution_options(stream_results=True).execute(sa.text(query))
+            if max_updated is None or self.update_all:
+                query = '''
+                    SELECT id, url
+                    FROM councilmatic_core_billdocument 
+                    WHERE document_type='A' 
+                    AND full_text is null
+                    ORDER BY updated_at DESC
+                    LIMIT 10
+                ''' 
+            else:
+                query = '''
+                    SELECT id, url
+                    FROM councilmatic_core_billdocument 
+                    WHERE updated_at >= :max_updated
+                    AND document_type='A' 
+                    AND full_text is null
+                    ORDER BY updated_at DESC
+                '''
+
+            result = connection.execution_options(stream_results=True).execute(sa.text(query), max_updated=max_updated)
 
             yield from result
 
@@ -64,32 +87,53 @@ class Command(BaseCommand):
             document_data = dict(document_data)
             url = document_data['url']
             document_id = document_data['id']
-           
-            if url.endswith('doc') or url.endswith('docx'):
-                try:
-                    # For Python 3.4 and below
-                    process = subprocess.Popen(['unoconv', '--stdout', '-f', 'text', url], preexec_fn=os.setsid, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.DEVNULL)
 
-                    plain_text, stderr_data = process.communicate(timeout=15)
+            response = requests.get(url)
+            extension = os.path.splitext(url)[1]
 
-                except subprocess.TimeoutExpired as e:
-                    os.killpg(os.getpgid(process.pid), signal.SIGTERM)
-                    logger.error(e)
-                    continue
+            # TODO: Is this possible without storing as temp file?
+            with open('/tmp/temp_file.{}'.format(extension), 'wb') as temp_pdf:
+                temp_pdf.write(response.content)
 
-                logger.info('Successful conversion of {}'.format(url))
+                plain_text = textract.process('/tmp/temp_file.{}'.format(extension))
 
-                yield {'plain_text': plain_text.decode('utf-8'), 'id': document_id}
+            # UNOCONV solution!            
+            # if url.endswith('doc') or url.endswith('docx'):
+            #     try:
+            #         # For Python 3.4 and below
+            #         process = subprocess.Popen(['unoconv', '--stdout', '-f', 'text', url], preexec_fn=os.setsid, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.DEVNULL)
+
+            #         plain_text, stderr_data = process.communicate(timeout=15)
+
+            #     except subprocess.TimeoutExpired as e:
+            #         os.killpg(os.getpgid(process.pid), signal.SIGTERM)
+            #         logger.error(e)
+            #         continue
+
+            #     logger.info('Successful conversion of {}'.format(url))
+
+            # pdfminer.six solution!
+            # elif url.lower().endswith('pdf'):
+            #     response = requests.get(url)
+
+            #     # TODO: Find a way to do this without a temp file.
+            #     with open('/tmp/temp_pdf.pdf', 'wb') as temp_pdf:
+            #         temp_pdf.write(response.content)
+
+            #     # Create a subprocess.Popen object
+            #     process = subprocess.Popen(['python', '/Users/reginacompton/.virtualenvs/a-test/bin/pdf2txt.py', '/tmp/temp_pdf.pdf'], preexec_fn=os.setsid, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.DEVNULL)
+
+            #     plain_text, stderr_data = process.communicate()
+
+            yield {'plain_text': plain_text.decode('utf-8'), 'id': document_id}
 
     def add_plain_text(self):
         plain_text_results = self.convert_document()
 
-        print(plain_text_results, "$$$")
-
         self.connection.execute("SET local timezone to '{}'".format(settings.TIME_ZONE))
         query = '''
             UPDATE councilmatic_core_billdocument as bill_docs
-            SET full_text = :full_text
+            SET full_text = :plain_text
             WHERE bill_docs.id = :id  
         '''
 
