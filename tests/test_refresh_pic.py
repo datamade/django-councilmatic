ec5af9fc6bfc1313ad609ed545b2d0d7369e78a9
reginafcompton <reginafcompton@gmail.com>
2018-10-12 13:00:16 -0500

    Remove magicmock

--- tests/test_refresh_pic.py
+++ tests/test_refresh_pic.py
@@ -1,6 +1,4 @@
 import pytest
-import sys
-from unittest.mock import MagicMock
 
 from django.db import connection
 
@@ -13,8 +11,6 @@ def test_command_functions(metro_bill_document, metro_change_bill):
     Test that the `_get_urls` and `_create_keys` successfully finds changed bill documents 
     and converts their urls to a list of AWS keys.
     '''
-    sys.modules['boto.s3.connection'] = MagicMock()
-
     doc = metro_bill_document
     refresh_pic_command = Command()
     document_urls = refresh_pic_command._get_urls()
