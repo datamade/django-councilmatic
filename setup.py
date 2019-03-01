8d64575cf93470d13db99db9f4d42afd3a67cc5c
Regina Compton <reginafcompton@gmail.com>
2019-03-01 09:51:01 -0500

    New release (#238)

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-councilmatic',
-    version='0.10.14',
+    version='0.10.15',
     packages=['councilmatic_core'],
     include_package_data=True,
     license='MIT License',  # example license
