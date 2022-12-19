2fe37e26bb01246a1f6c86ffd9722be0f91a3580
hannah cushman garland <hannah.cushman@gmail.com>
2022-12-19 11:52:35 -0600

    Fix person template

--- setup.py
+++ setup.py
@@ -40,7 +40,10 @@ setup(
         "License :: OSI Approved :: BSD License",  # example license
         "Operating System :: OS Independent",
         "Programming Language :: Python",
-        # Replace these appropriately if you are stuck on Python 2.
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
