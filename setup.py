10d46c3fa09b27fe7b46ecb046e2c3de393c83ed
hannah cushman garland <hannah.cushman@gmail.com>
2023-04-05 13:08:58 -0500

    Prepare for 3.2 release

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name="django-councilmatic",
     python_requires=">=3.6",
-    version="3.1",
+    version="3.2",
     packages=find_packages(exclude=("tests",)),
     include_package_data=True,
     license="MIT License",  # example license
