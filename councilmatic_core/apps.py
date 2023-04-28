82978d08e99e78ac34b4f8d7fd636fed1ad88285
removedporn <86824510+removedporn@users.noreply.github.com>
2023-04-28 16:15:47 +0800

    Delete councilmatic_core directory

--- councilmatic_core/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class CouncilmaticConfig(AppConfig):
-    name = "councilmatic_core"
-    verbose_name = "Councilmatic"
-
-    def ready(self):
-        import councilmatic_core.signals.handlers  # noqa
